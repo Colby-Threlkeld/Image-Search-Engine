5c647443f5c0920d6b8529df92b93942c5bd89ea
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-04-12 13:59:58 -0400

    Initital Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById('search-form');
+const searchBar = document.getElementById('search-bar');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+
