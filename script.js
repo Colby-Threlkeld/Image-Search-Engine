66836b034a6f2ff35ddf5709ecb0a6289169f9fd
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-04-12 15:31:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let page = 1;
 
 async function searchImages(){
     keyword = searchBar.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const url = `/.netlify/functions/search?query=${keyword}&page=${page}`;
+
 
     const response = await fetch(url);
     const data = await response.json();
